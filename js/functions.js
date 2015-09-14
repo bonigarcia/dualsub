dccc495426d383618c93bdd6f1281823d3a1a437
Boni Garcia <bgarcia@gsyc.es>
2015-09-14 20:40:29 +0200

    Version 1.0.0 of DualSub web site

---
+++ js/functions.js
@@ -0,0 +1,11 @@
+
+$(window).load(function() {
+	$('.flexslider').flexslider({
+		animation: "slide",
+		controlsContainer: ".slider-holder",
+		slideshowSpeed: 5000,
+		directionNav: true,
+		controlNav: false,
+		animationDuration: 900
+	});
+});
